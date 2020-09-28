331ff908b6c51ea16504a9cb5c679e108fec2d24
akashgupta3852 <akashgupta3852@gmail.com>
2020-09-28 20:57:33 +0530

    [Akash]Added: check whether pin is valid or not

--- src/com/pinCode/PinCode.java
+++ src/com/pinCode/PinCode.java
@@ -1,9 +1,25 @@
 package com.pinCode;
 
+import java.util.regex.*;
+import java.util.*;
+
 public class PinCode {
+	
+	public static boolean isValidPinCode(String pinCode) {
+		String regex="[1-9][0-9]{5}";
+		Pattern pattern=Pattern.compile(regex);
+		return pattern.matcher(pinCode).matches();
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Pin Code Problem");
+
+		System.out.println("Enter the pin code:");
+		Scanner scan=new Scanner(System.in);
+		String pin=scan.next();
+		
+		System.out.println("The pin code: "+pin+" is "+isValidPinCode(pin));
+		
 	}
 
 }
